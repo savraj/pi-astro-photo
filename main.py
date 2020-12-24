89d3e5b5b762486918309e8877fcf90879be543c
Savraj Singh <savraj@gmail.com>
2020-12-24 13:41:41 -0500

    current statu

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ print("program starting up. Press ENTER to capture an image, CTRL-C to quit.")
 with PiCamera() as camera:
     camera.start_preview()
     try:
-        for i, filename in enumerate(
-                camera.capture_continuous('img_{timestamp:%H-%M-%S}-{counter:03d}.jpg')):
+        for i, filename in enumerate(camera.capture_continuous('img_{timestamp:%H-%M-%S}-{counter:03d}.jpg')):
             print(filename)
             time.sleep(2)
-            input("press Enter to snap an image.")
+            k = input("press Enter to snap an image.")
     finally:
         camera.stop_preview()
+
